e161ad349f86368b48f2471a085a77b5b655a2ce
Miles Cochran-Branson <mcochranbranson@gmail.com>
2025-04-18 10:15:30 -0700

    update icons

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,17 @@
-import TwitterIcon from '@/components/icons/TwitterIcon'
-import GithubIcon from '@/components/icons/GithubIcon'
+import InstagramIcon from '@/components/icons/InstagramIcon' // <-- Import new icon
+import FacebookIcon from '@/components/icons/FacebookIcon' // <-- Import new icon
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
-		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: GithubIcon
+		name: 'Instagram',
+		url: 'https://instagram.com/kyles_23', // <-- Update URL
+		icon: InstagramIcon
 	},
 
 	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
+		name: 'Facebook',
+		url: 'https://facebook.com/your_page_or_profile', // <-- Update URL
+		icon: FacebookIcon
 	}
 ] as const
